cc432920f14e6df1aadf13f6a8507756f0aedbc1
Nathan Andrade <nathan.nhdsk@gmail.com>
2023-04-15 20:47:37 -0300

    add init file and rename module for test

--- __tests__/fileparser_test.py
+++ __tests__/fileparser_test.py
@@ -1,17 +1,10 @@
 import sys
-sys.path.append('../src')
+sys.path.append('../src/fileparser.py')
+from src.fileparser import FileParser
 
-import unittest
-from fileparser import FileParser
-
-class FileParserTest(unittest.TestCase):
-    def test_read_file(self):
-        words_list = FileParser("./resources/words_test").read_file()
-        self.assertEqual(words_list, [
-        {"word": "(transliteration1) meaning for testing"},
-        {"new word": "(transliteration2) second meaning for another testing"}
-    ])
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_read_file():
+    words_list = FileParser("./resources/words_test").read_file()
+    assert words_list == [
+    {"word": "(transliteration1) meaning for testing"},
+    {"new word": "(transliteration2) second meaning for another testing"}
+]
