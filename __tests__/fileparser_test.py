64546044794ea8825480a4a52994fd45e5af7a46
Nathan Andrade <nathan.nhdsk@gmail.com>
2023-04-15 20:38:03 -0300

    rewrite module import

--- __tests__/fileparser_test.py
+++ __tests__/fileparser_test.py
@@ -1,11 +1,11 @@
 import unittest
 import sys
 sys.path.append('../src/fileparser.py')
-from fileparser import FileParser
+import fileparser
 
 class FileParserTest(unittest.TestCase):
     def test_read_file(self):
-        words_list = FileParser("./resources/words_test").read_file()
+        words_list = fileparser.FileParser("./resources/words_test").read_file()
         print(words_list)
         self.assertEqual(words_list, [
         {"word": "(transliteration1) meaning for testing"},
