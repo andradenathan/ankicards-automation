a69a0f2bb1985a3ac82877109676bedb4bf71a59
Nathan Andrade <nathan.nhdsk@gmail.com>
2023-04-15 20:50:55 -0300

    rename path folder to the parser constructor

--- __tests__/fileparser_test.py
+++ __tests__/fileparser_test.py
@@ -1,9 +1,7 @@
-import sys
-sys.path.append('../src/fileparser.py')
 from src.fileparser import FileParser
 
 def test_read_file():
-    words_list = FileParser("./resources/words_test").read_file()
+    words_list = FileParser("./__tests__/resources/words_test").read_file()
     assert words_list == [
     {"word": "(transliteration1) meaning for testing"},
     {"new word": "(transliteration2) second meaning for another testing"}
