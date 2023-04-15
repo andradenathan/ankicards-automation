46ab0704e4dd0c857e9683e72f2014d0c6fa174e
Nathan Andrade <nathan.nhdsk@gmail.com>
2023-04-15 20:25:12 -0300

    add tests for file parsing

---
+++ __tests__/fileparser_test.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../src')
+
+import unittest
+from fileparser import FileParser
+
+class FileParserTest(unittest.TestCase):
+    def test_read_file(self):
+        words_list = FileParser("./resources/words_test").read_file()
+        print(words_list)
+        self.assertEqual(words_list, [
+        {"word": "(transliteration1) meaning for testing"},
+        {"new word": "(transliteration2) second meaning for another testing"}
+    ])
+
+
+if __name__ == '__main__':
+    unittest.main()
